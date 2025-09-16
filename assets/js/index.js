9be5f6fd29907dd94be43dcea808d4243f5f9eec
Sergio Roman <u22319658@utp.edu.pe>
2025-09-16 00:19:39 -0500

    añadiendo js

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+
+// Inicialización de iconos Lucide
+lucide.createIcons();
+
+// Version Movil
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
